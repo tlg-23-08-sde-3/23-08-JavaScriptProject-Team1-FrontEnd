2c6576df98888b2dc7a45000b847550e3b284bee
Sarthak Oza <Sarthak.Oza@tlgcohort.com>
2023-10-18 13:33:13 -0700

    search functionality added

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ const jobsListContainer = document.getElementById("jobs-list-container");
 async function fetchJobs() {
    const fetchedJobs = await fetch(apiUrl);
    const jobs = await fetchedJobs.json();
+   console.log(jobs);
 
    jobs.forEach((job) => {
       const cardDiv = Object.assign(document.createElement("div"), {
@@ -23,7 +24,12 @@ async function fetchJobs() {
       cardText.classList.add("card-text");
 
       const slicedJobDescription = job.contents.slice(0, 200);
-      cardText.innerHTML = slicedJobDescription;
+      cardText.innerHTML = `
+         
+         <p>${job.pay}</p>
+         <p>${job.location}</p>
+         ${slicedJobDescription}
+      `;
 
       cardDiv.innerHTML = `
       <div class="card-body">
@@ -49,6 +55,10 @@ async function fetchJobs() {
       pageLink.classList.add("page-item");
       pageLink.innerHTML = `<a class="page-link" href="#">${i}</a>`;
       document.querySelector(".pagination").appendChild(pageLink);
+
+      if (i === 1) {
+         pageLink.classList.add("active");
+      }
    }
 
    // jobs on current page
@@ -95,7 +105,7 @@ async function fetchJobs() {
       });
 
       const secondPageItem = document.querySelector(
-         `.pagination .page-item:nth-child(${currentPage })`
+         `.pagination .page-item:nth-child(${currentPage})`
       );
 
       secondPageItem.classList.add("active");
@@ -123,7 +133,7 @@ async function displayFirstJobDetails(id) {
    singleJobView.innerHTML = `
    <h5>${job.name}</h5>
    <p>${job.contents}</p>
-   <button type="button" class="btn btn-primary" onclick="window.location.href=#" target="_blank" rel="noopener noreferrer">Apply</button> 
+   <button type="button" class="btn btn-primary" target="_blank" rel="noopener noreferrer">Apply</button> 
 
    `;
    jobViewContainer.innerHTML = "";
@@ -153,11 +163,160 @@ jobsListContainer.addEventListener("click", async (e) => {
    singleJobView.innerHTML = `
    <h5>${job.name}</h5>
    <p>${job.contents}</p>
-   <button type="button" class="btn btn-primary" onclick="window.location.href=#" target="_blank" rel="noopener noreferrer">Apply</button>
+   <button type="button" class="btn btn-primary" target="_blank" rel="noopener noreferrer">Apply</button>
 
    `;
    jobViewContainer.innerHTML = "";
    jobViewContainer.appendChild(singleJobView);
 });
+
+document
+   .getElementById("search-jobs-form")
+   .addEventListener("submit", async (e) => {
+      e.preventDefault();
+      console.log("triggwered");
+
+      const jobRoleInput = document.getElementById("role-search-input").value;
+      const locationInput = document.getElementById(
+         "location-search-input"
+      ).value;
+      const keywordsInput = document.getElementById(
+         "keywords-search-input"
+      ).value;
+
+      console.log(jobRoleInput, locationInput);
+
+      const query = {};
+
+      if (jobRoleInput) {
+         query.name = jobRoleInput;
+      }
+
+      if (locationInput) {
+         query.location = locationInput;
+      }
+
+      if (keywordsInput) {
+         query.keywords = keywordsInput;
+      }
+
+      //create query string
+      const queryString = new URLSearchParams(query).toString();
+
+      console.log(apiUrl + `?${queryString}`);
+
+      const fetchedJobs = await fetch(apiUrl + `?${queryString}`);
+      const jobs = await fetchedJobs.json();
+      console.log(jobs);
+
+      jobsListContainer.innerHTML = "";
+      document.querySelector(".pagination").innerHTML = "";
+
+      jobs.forEach((job) => {
+         const cardDiv = Object.assign(document.createElement("div"), {
+            classList: "card",
+            style: "width: 50rem",
+            id: job._id,
+         });
+
+         cardDiv.dataset.jobID = job.id;
+
+         // trimmed description p element
+         const cardText = document.createElement("p");
+         cardText.classList.add("card-text");
+
+         const slicedJobDescription = job.contents.slice(0, 200);
+         cardText.innerHTML = `
+            
+            <p>${job.pay}</p>
+            <p>${job.location}</p>
+            ${slicedJobDescription}
+         `;
+
+         cardDiv.innerHTML = `
+         <div class="card-body">
+         <h5 class="card-title">${job.name}</h5>
+       </div>
+         `;
+         cardDiv.querySelector(".card-body").appendChild(cardText);
+
+         jobsListContainer.appendChild(cardDiv);
+      });
+
+      //pagination handling
+      const itemsPerPage = 5;
+
+      let currentPage = 1;
+
+      // Calculate the number of total pages based on the number of jobs
+      const totalPages = Math.ceil(jobs.length / itemsPerPage);
+
+      // Create the pagination links dynamically
+      for (let i = 1; i <= totalPages; i++) {
+         const pageLink = document.createElement("li");
+         pageLink.classList.add("page-item");
+         pageLink.innerHTML = `<a class="page-link" href="#">${i}</a>`;
+         document.querySelector(".pagination").appendChild(pageLink);
+
+         if (i === 1) {
+            pageLink.classList.add("active");
+         }
+      }
+
+      // jobs on current page
+      async function showPage(page) {
+         const jobCards = $(".card");
+         console.log(jobCards);
+
+         const start = (page - 1) * itemsPerPage;
+         const end = start + itemsPerPage;
+
+         jobCards.hide();
+         jobCards.slice(start, end).show();
+         // calling function to display first job
+
+         await displayFirstJobDetails(jobCards.slice(start, end)[0].id);
+         // add "active" class for highlight color"
+      }
+
+      showPage(currentPage);
+
+      // Handle pagination click events
+      $(".pagination").on("click", "a.page-link", function (e) {
+         e.preventDefault();
+         const text = $(this).text();
+         if (text === "Previous") {
+            if (currentPage > 1) {
+               currentPage--;
+               showPage(currentPage);
+            }
+         } else if (text === "Next") {
+            if (currentPage < Math.ceil($(".card").length / itemsPerPage)) {
+               currentPage++;
+               showPage(currentPage);
+            }
+         } else {
+            currentPage = parseInt(text);
+            showPage(currentPage);
+         }
+
+         // check for active number, remove active class and add for current page number
+         const pageItems = document.querySelectorAll(".pagination .page-item");
+
+         pageItems.forEach((pageItem) => {
+            pageItem.classList.remove("active");
+         });
+
+         const secondPageItem = document.querySelector(
+            `.pagination .page-item:nth-child(${currentPage})`
+         );
+
+         secondPageItem.classList.add("active");
+      });
+
+      document.getElementById("role-search-input").value = "";
+      document.getElementById("location-search-input").value = "";
+      document.getElementById("keywords-search-input").value = "";
+   });
 
 fetchJobs();
